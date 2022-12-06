3166fed707086be0b2961a8aabd318b56189cd85
Woonhak Kong <101300258@georgebrown.ca>
2022-12-05 19:20:31 -0500

    CharacterController is implemented for inherit

---
+++ Assets/[Scripts]/PlayScene/CharacterController.cs
@@ -0,0 +1,206 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Mathematics;
+using System;
+
+public enum State
+{
+    IDLE,
+    RUN,
+    JUMP_UP,
+    JUMP_DOWN,
+    ATTACK,
+    HIT,
+}
+
+
+public class CharacterController : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpFactor;
+    [SerializeField] private bool isOnGround;
+    [SerializeField] private bool isStickyToWall;
+    [SerializeField] private bool firstJump;
+    [SerializeField] private bool doubleJump;
+    [SerializeField] protected bool isAttacking;
+
+
+    [SerializeField] private BoxCollider2D groundCheckBox;
+    [SerializeField] private BoxCollider2D forwardCheckBox;
+    [SerializeField] protected BoxCollider2D attackBox;
+
+    [SerializeField] private State currentState;
+
+    private Animator animator;
+
+
+    public ContactFilter2D groundContactFilter;
+    
+
+    private Rigidbody2D rigidbody2D;
+
+    protected Vector2 characterDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentState = State.IDLE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateIsOnGround();
+        UpdateIsStickyToWall();
+        UpdateState();
+
+        //SetAnimation();
+    }
+
+    private void UpdateState()
+    {
+        if (isAttacking)
+        {
+            currentState = State.ATTACK;
+        }
+        else if (rigidbody2D.velocity.y > 0.0f && !isOnGround)
+        {
+            currentState = State.JUMP_UP;
+        }
+        else if (rigidbody2D.velocity.y < 0.0f && !isOnGround)
+        {
+            currentState = State.JUMP_DOWN;
+        }
+        else if (rigidbody2D.velocity.x != 0.0f)
+        {
+            currentState = State.RUN;
+        }
+        else if (rigidbody2D.velocity.x == 0.0f)
+        {
+            currentState = State.IDLE;
+        }
+
+        animator.SetInteger("factor", (int)currentState);
+    }
+
+
+    private void FixedUpdate()
+    {
+        DoBehavior();
+    }
+
+    private void DoBehavior()
+    {
+        // Moving
+        float velocityX = characterDirection.x * speed * Time.fixedDeltaTime;
+
+        if (velocityX < 0.0f)
+        {
+            transform.localScale = new Vector2(-1.0f, transform.localScale.y);
+        }
+        else if (velocityX > 0.0f)
+        {
+            transform.localScale = new Vector2(1.0f, transform.localScale.y);
+        }
+
+
+        if (isStickyToWall)
+        {
+            rigidbody2D.velocity = new Vector2(0.0f, rigidbody2D.velocity.y);
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(velocityX, rigidbody2D.velocity.y);
+        }
+
+    }
+
+    private void UpdateIsOnGround()
+    {
+        List<Collider2D> collisions = new List<Collider2D>();
+        int result = groundCheckBox.OverlapCollider(groundContactFilter, collisions);
+        isOnGround = result > 0;
+        if (isOnGround && rigidbody2D.velocity.y <= 0.5f)
+        {
+            firstJump = false;
+            doubleJump = false;
+        }
+    }
+
+    private void UpdateIsStickyToWall()
+    {
+        List<Collider2D> collisions = new List<Collider2D>();
+        int result = forwardCheckBox.OverlapCollider(groundContactFilter, collisions);
+        isStickyToWall = result > 0;
+
+    }
+
+    protected void SetDirection(Vector2 direction)
+    {
+        characterDirection = direction;
+    }
+
+    // input system
+    public void Jump()
+    {
+        if (!firstJump)
+        {
+            firstJump = true;
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0.0f);
+            rigidbody2D.AddForce(Vector2.up * jumpFactor, ForceMode2D.Impulse);
+        }
+        else if (!doubleJump)
+        {
+            doubleJump = true;
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0.0f);
+            rigidbody2D.AddForce(Vector2.up * jumpFactor, ForceMode2D.Impulse);
+        }
+    }
+
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+
+        // checking ground
+        float3 groundCheckPosition = new float3(groundCheckBox.gameObject.transform.position.x + groundCheckBox.offset.x,
+            groundCheckBox.gameObject.transform.position.y + groundCheckBox.offset.y, 0.0f);
+        Gizmos.DrawWireCube(groundCheckPosition, groundCheckBox.size);
+
+        // checking forward
+        float3 forwardCheckPosition = new float3(forwardCheckBox.gameObject.transform.position.x + forwardCheckBox.offset.x,
+            forwardCheckBox.gameObject.transform.position.y + forwardCheckBox.offset.y, 0.0f);
+        Gizmos.DrawWireCube(forwardCheckPosition, forwardCheckBox.size);
+    }
+
+    //public void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+    //    {
+    //    }
+    //}
+
+    public void SetIsAttackingFalse()
+    {
+        isAttacking = false;
+    }
+
+    public void Attack()
+    {
+        isAttacking = true;
+
+    }
+
+    public virtual void AttackEventForHit()
+    {
+
+    }
+
+
+}
--- Assets/[Scripts]/PlayScene/PlayerController.cs
+++ Assets/[Scripts]/PlayScene/PlayerController.cs
@@ -2,201 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Unity.Mathematics;
-using System;
 
-public enum PlayerState
+public class PlayerController : CharacterController
 {
-    IDLE,
-    RUN,
-    JUMP_UP,
-    JUMP_DOWN,
-    ATTACK,
-    HIT,
-}
+    public ContactFilter2D attackContactFilter;
 
 
-public class PlayerController : MonoBehaviour
-{
 
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpFactor;
-    [SerializeField] private bool isOnGround;
-    [SerializeField] private bool isStickyToWall;
-    [SerializeField] private bool firstJump;
-    [SerializeField] private bool doubleJump;
-    
-
-    [SerializeField] private BoxCollider2D groundCheckBox;
-    [SerializeField] private BoxCollider2D forwardCheckBox;
-
-    [SerializeField] private PlayerState currentState;
-
-    private Animator animator;
-
-
-    public ContactFilter2D groundContactFilter;
-
-    private Rigidbody2D rigidbody2D;
-
-    private Vector2 characterDirection;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public void OnClickAttack(InputAction.CallbackContext context)
     {
-        rigidbody2D= GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        currentState = PlayerState.IDLE;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateIsOnGround();
-        UpdateIsStickyToWall();
-        UpdatePlayerState();
-
-        //SetAnimation();
-    }
-
-    private void UpdatePlayerState()
-    {
-        if (rigidbody2D.velocity.y > 0.0f && !isOnGround)
+        if (context.performed)
         {
-            currentState = PlayerState.JUMP_UP;
-        }
-        else if(rigidbody2D.velocity.y < 0.0f && !isOnGround)
-        {
-            currentState = PlayerState.JUMP_DOWN;
-        }
-        else if(rigidbody2D.velocity.x != 0.0f)
-        {
-            currentState = PlayerState.RUN;
-        }
-        else if(rigidbody2D.velocity.x == 0.0f)
-        {
-            currentState = PlayerState.IDLE;
-        }
-
-        animator.SetInteger("factor", (int)currentState);
-    }
-
-    private void SetAnimation()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void FixedUpdate()
-    {
-        DoBehavior();
-    }
-
-    private void DoBehavior()
-    {
-        // Moving
-        float velocityX = characterDirection.x * speed * Time.fixedDeltaTime;
-
-        if (velocityX < 0.0f)
-        {
-            transform.localScale = new Vector2(-1.0f, transform.localScale.y);
-        }
-        else if(velocityX > 0.0f)
-        {
-            transform.localScale = new Vector2(1.0f, transform.localScale.y);
-        }
-     
-
-        if (isStickyToWall)
-        {
-            rigidbody2D.velocity = new Vector2(0.0f, rigidbody2D.velocity.y);
-        }
-        else
-        {
-            rigidbody2D.velocity = new Vector2(velocityX, rigidbody2D.velocity.y);
-        }
-
-    }
-
-    private void UpdateIsOnGround()
-    {
-        List<Collider2D> collisions = new List<Collider2D>();
-        int result = groundCheckBox.OverlapCollider(groundContactFilter, collisions);
-        isOnGround = result > 0;
-        if (isOnGround && rigidbody2D.velocity.y <= 0.5f)
-        {
-            firstJump = false;
-            doubleJump = false;
+            Attack();
         }
     }
-
-    private void UpdateIsStickyToWall()
-    {
-        List<Collider2D> collisions = new List<Collider2D>();
-        int result = forwardCheckBox.OverlapCollider(groundContactFilter, collisions);
-        isStickyToWall = result > 0;
-       
-    }
-
-
 
     // input system
-    public void Move(InputAction.CallbackContext context)
+    public void OnClickMove(InputAction.CallbackContext context)
     {
         Debug.Log(context.ReadValue<Vector2>());
-        characterDirection = context.ReadValue<Vector2>();
+        SetDirection(context.ReadValue<Vector2>());
     }
 
     // input system
-    public void Jump(InputAction.CallbackContext context)
+    public void OnClickJump(InputAction.CallbackContext context)
     {
         // only pressed
         if (context.performed)
         {
-            //if (isOnGround && !firstJump)
-            //{
-            //    Debug.Log("jump = " + context.ReadValueAsButton());
-            //    firstJump = true;
-            //    rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0.0f);
-            //    rigidbody2D.AddForce(Vector2.up * jumpFactor, ForceMode2D.Impulse);
-            //}
-            if (!firstJump)
-            {
-                firstJump = true;
-                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0.0f);
-                rigidbody2D.AddForce(Vector2.up * jumpFactor, ForceMode2D.Impulse);
-            }
-            else if (!doubleJump)
-            {
-                doubleJump = true;
-                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0.0f);
-                rigidbody2D.AddForce(Vector2.up * jumpFactor, ForceMode2D.Impulse);
-            }
+            Jump();
         }
-        
     }
 
-    public void OnDrawGizmos()
+    public override void AttackEventForHit()
     {
-        Gizmos.color = Color.magenta;
+        attackBox.size = new Vector2(2.0f, 2.0f);
+        attackBox.isTrigger = true;
 
-        // checking ground
-        float3 groundCheckPosition = new float3(groundCheckBox.gameObject.transform.position.x + groundCheckBox.offset.x,
-            groundCheckBox.gameObject.transform.position.y + groundCheckBox.offset.y, 0.0f);
-        Gizmos.DrawWireCube(groundCheckPosition, groundCheckBox.size);
-
-        // checking forward
-        float3 forwardCheckPosition = new float3(forwardCheckBox.gameObject.transform.position.x + forwardCheckBox.offset.x,
-            forwardCheckBox.gameObject.transform.position.y + forwardCheckBox.offset.y, 0.0f);
-        Gizmos.DrawWireCube(forwardCheckPosition, forwardCheckBox.size);
-    }
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        List<Collider2D> collisions = new List<Collider2D>();
+        int result = attackBox.GetComponent<BoxCollider2D>().OverlapCollider(attackContactFilter, collisions);
+        if (result > 0)
         {
+            Debug.Log("collision with " + collisions[0].name);
         }
     }
-
-
 }
