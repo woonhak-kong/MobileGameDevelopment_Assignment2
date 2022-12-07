6aeb256712a0b02fed5cc5c1d4d9e2fcafe6addc
Woonhak Kong <101300258@georgebrown.ca>
2022-12-06 23:31:56 -0500

    instruction is added

--- Assets/[Scripts]/PlayScene/PlayerController.cs
+++ Assets/[Scripts]/PlayScene/PlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class PlayerController : CharacterController
 {
